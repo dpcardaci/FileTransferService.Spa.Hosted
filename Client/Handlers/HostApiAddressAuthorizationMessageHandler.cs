7e4477e18e53b802eb2411de8ea7d0bd678c9f51
David Cardaci <dacardac@microsoft.com>
2023-08-03 16:26:55 -0400

    moved the scope into appsettings

--- Client/Handlers/HostApiAddressAuthorizationMessageHandler.cs
+++ Client/Handlers/HostApiAddressAuthorizationMessageHandler.cs
@@ -6,12 +6,13 @@ namespace FileTransferService.Spa.Hosted.Client.Handlers
     public class HostApiAddressAuthorizationMessageHandler : AuthorizationMessageHandler
     {
         public HostApiAddressAuthorizationMessageHandler(IAccessTokenProvider provider,
-            NavigationManager navigationManager)
+            NavigationManager navigationManager, IConfiguration configuration)
             : base(provider, navigationManager)
         {
             ConfigureHandler(
                 authorizedUrls: new[] { navigationManager.BaseUri },
-                scopes: new[] { "api://60a0d398-e5ab-43c6-a01f-f27dd8f6ab59/API.Access" }
+                scopes: configuration.GetSection("FileTransferServiceHost:Scopes")
+                            .Get<List<string>>()
             );
         }
     }
